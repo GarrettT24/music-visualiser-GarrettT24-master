7953166027353d431f7e4a22253772ba5ac8bb90
GarrettT24 <brightandr1@myvuw.ac.nz>
2024-09-11 11:38:03 +1200

    Commit 8
    Creating array to animate

--- music_starter.js
+++ music_starter.js
@@ -1,7 +1,11 @@
 let firstRun = true
+
 let img;
 let img2;
 let img3;
+let img4;
+
+let noo = [];
 
 // vocal=bass, drum=snare, bass=kick, other=hihat
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
@@ -9,20 +13,33 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   // rectMode(CENTER)
   if (firstRun){ //images
     img = loadImage('/assets/mugs.png'); //mugshots picture
-    img2 = loadImage('/assets/wheel1.png'); //wheel 1
-    img3 = loadImage('/assets/strip1.png'); //strip 1
+    img2 = loadImage('/assets/wheel1.png'); //
+    img3 = loadImage('/assets/strip1.png'); //Russel Hobbs animation taken from 'https://tenor.com/view/gorillaz-russel-hobbes-russel-russel-gorillaz-gorillaz-drummer-gif-19558804'
+    img4 = loadImage('/assets/noo1.png'); //Noodle animation taken from 'https://tenor.com/view/noodle-rotate-spin-spinning-gorillaz-gif-18012651'
+
+    // noo.push(loadImage('/assets/noo1.png'));
+    noo.push(loadImage('noo1.png'));
+
     firstRun = false
   }
 
-  frameRate(60)
-
-  // let hihat = map(bass, 0, 100, 0, 10)
+  frameRate(8)
   
-  background(255);
+  background(0);
 
-  let snare = map(drum, 0, 80, 0, 10000);
+  //port hole = 420*400
 
-  image(img3, 1050, snare -2000);// image(img, -960, -540);
+  //image(img2, 448, 156)//reference image is 1024x768 / 2 = 512x384, 1920x1080 / 2 = 960x540
+  
+  push();
+  rect(1020, 80, 420, 400);
+  let snare = map(drum, 0, 70, 0, 23458);
+  pop();
+
+  push();
+  translate(1050, 77);
+  image(img3, 0, 0 - snare);//Hobbs, image(img, -960, -540);
+  pop();
 
   let kick = map(bass, 0, 80, 0, 360);
 
@@ -33,14 +50,20 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   image(img2, 0, 0);//wheel 1
   pop();
 
-  //image(img2, 448, 156)//reference image is 1024x768 / 2 = 512x384, 1920x1080 / 2 = 960x540
-  
-  //port hole = 480*270
+  var hihat = map (vocal, 0, 100, 0, 7);
+
+  rect(480, 600, 420, 400);
+
+  push();
+  scale(1.5);
+  // image(img4, 360, 425)// noodle
+  image(noo[hihat], width/2, height/2)
+  pop();
 
   fill(1, 24, 37);
   strokeWeight(0);
   stroke(255);
-  rect(0, 0, 1920, 225);//top horizontal
+  rect(0, 0, 1920, 80);//top horizontal
 
   rect(0, 0, 480, 1080);//left vertical
 
@@ -50,17 +73,17 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   fill(1, 24, 37);
   strokeWeight(0);
   stroke(255);
-  rectMode(CENTER)
+  rectMode(CENTER);
   rect(960, 0, 120, 2160);//center vertical
   
   fill(1, 24, 37);
   strokeWeight(0);
   stroke(255);
-  rectMode(CENTER)
+  rectMode(CENTER);
   rect(0, 540, 3840, 120);//center horizontal
   pop();
   
-  rect(0, 855, 1920, 225);//bottom horizontal
+  rect(0, 1000, 1920, 80);//bottom horizontal
 
   fill(255);
   ellipse(1720, 1080, 10, 10);//reference dot
